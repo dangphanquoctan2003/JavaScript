a8584c78610ff211169f7d703e05709d0cb824b4
dangtan <dangphanquoctan2003@gmail.com>
2024-06-15 17:46:19 +0700

    not finish yet

---
+++ Day06_learn--htmlDom/main.js
@@ -0,0 +1,10 @@
+// HTML DOM //
+// 1. Element: ID, class, tag, // CSS selector, HTML collection
+// 2. Attribute
+// 3. Text
+var productsListElement = document.querySelector
+('.products-list');
+var firstProductElement =document.querySelector
+(' .product:first-child');
+var buttonElements =document.getElementsByTagName('button');
+
